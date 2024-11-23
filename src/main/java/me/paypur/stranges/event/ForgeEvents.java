a3b0cd91ed99a00864ec47e6d949958fde83e19d
paypur <pieceofpaypur@gmail.com>
2024-11-23 17:36:26 -0500

    Basic tag writing
    Took 2 hours 10 minutes

--- src/main/java/me/paypur/stranges/event/ForgeEvents.java
+++ src/main/java/me/paypur/stranges/event/ForgeEvents.java
@@ -2,14 +2,15 @@ package me.paypur.stranges.event;
 
 import me.paypur.stranges.Stranges;
 import net.minecraft.ChatFormatting;
-import net.minecraft.client.Minecraft;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.ListTag;
 import net.minecraft.nbt.LongTag;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.TextComponent;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
-import net.minecraftforge.event.entity.player.AttackEntityEvent;
+import net.minecraftforge.event.entity.living.LivingDeathEvent;
 import net.minecraftforge.event.entity.player.ItemTooltipEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
@@ -22,29 +23,49 @@ public class ForgeEvents {
     void tooltip(ItemTooltipEvent event) {
 
         if (event.getItemStack().getItem().equals(Items.WOODEN_AXE)) {
-
             CompoundTag tag = event.getItemStack().getTag();
 
             // CompoundTag id is 10
-            ListTag e = tag != null ? tag.getList("Strange", 10) : new ListTag();
+            ListTag tags = tag != null ? tag.getList("Strange", 10) : new ListTag();
 
-            long kills = ((LongTag) e.getCompound(0).get("kills")).getAsLong();
+            long kills = ((LongTag) tags.getCompound(0).get("kills")).getAsLong();
 
-            List<Component> x = event.getToolTip();
-            x.add(TextComponent.EMPTY);
-            x.add((new TextComponent(String.format("%s - Kills: %d", Stranges.rank(kills), kills)).withStyle(ChatFormatting.GRAY)));
-
-//            for(int i = 0; i < e.size(); ++i) {
-//            }
+            List<Component> components = event.getToolTip();
+            components.add(TextComponent.EMPTY);
+            components.add((new TextComponent(String.format("%s - Kills: %d", Stranges.rank(kills), kills)).withStyle(ChatFormatting.GRAY)));
 
         }
 
     }
 
     @SubscribeEvent
-    void attack(AttackEntityEvent event) {
-        if (event.getTarget().isAlive()) {
-            Minecraft.getInstance().player.chat("ded");
+    void attack(LivingDeathEvent event) {
+        if (event.getSource().getEntity() instanceof Player player) {
+            ItemStack item = player.getMainHandItem();
+
+            CompoundTag tag = item.getTag();
+
+            if (tag == null) {
+                return;
+            }
+
+            if (tag.getList("Strange", 10).isEmpty()) {
+                tag.put("Strange", new ListTag());
+            }
+
+            ListTag strange = tag.getList("Strange", 10);
+
+            if (strange.isEmpty()) {
+                CompoundTag killsTag = new CompoundTag();
+                killsTag.putLong("kills", 0);
+                strange.add(killsTag);
+            }
+
+            CompoundTag killsTag = (CompoundTag) strange.get(0);
+            long kills = ((LongTag) killsTag.get("kills")).getAsLong();
+            killsTag.putLong("kills", kills + 1);
+
+            tag.put("Strange", strange);
         }
     }
 
